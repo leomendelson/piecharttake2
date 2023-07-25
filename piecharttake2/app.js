c97473bc2a41b0bcde881a1067afc3cca8bd77a4
Leo Mendelson <lmendelson98@gmail.com>
2023-07-25 10:54:31 -0500

    data set up

--- piecharttake2/app.js
+++ piecharttake2/app.js
@@ -1,6 +1,6 @@
 async function draw() {
   // Data
-  const dataset = await d3.csv('data.csv')
+  const dataset = await d3.csv("data.csv");
 
   // Dimensions
   let dimensions = {
@@ -9,22 +9,29 @@ async function draw() {
     margins: 10,
   };
 
-  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
-  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2;
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2;
 
   // Draw Image
-  const svg = d3.select('#chart')
+  const svg = d3
+    .select("#chart")
     .append("svg")
     .attr("width", dimensions.width)
-    .attr("height", dimensions.height)
+    .attr("height", dimensions.height);
 
-  const ctr = svg.append("g") // <g>
+  const ctr = svg
+    .append("g") // <g>
     .attr(
       "transform",
       `translate(${dimensions.margins}, ${dimensions.margins})`
-    )
+    );
 
   // Scales
+  const populationPie = d3.pie().value((d) => d.value);
+
+  const slices = populationPie(dataset);
+
+  console.log(slices);
 }
 
-draw()
+draw();
