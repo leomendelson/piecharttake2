60eaae72cd50b9ddbbb3ca920d25d52dd563c0af
Leo Mendelson <lmendelson98@gmail.com>
2023-08-05 20:00:38 -0500

    fewer data points and new colors

--- piecharttake2/app.js
+++ piecharttake2/app.js
@@ -1,7 +1,16 @@
 async function draw() {
   // Data
-  const dataset = await d3.csv("data.csv");
-
+  const dataset = [
+    { name: "<5", value: "19912018" },
+    { name: "5-9", value: "20501982" },
+    { name: "10-14", value: "20679786" },
+    { name: "15-19", value: "21354481" },
+    { name: "20-24", value: "22604232" },
+    { name: "25-29", value: "21698010" },
+  ];
+  // await d3.csv("data.csv");
+  // console.log(dataset);
+  // console.log(JSON.stringify(dataset));
   // Dimensions
   let dimensions = {
     width: 600,
@@ -32,6 +41,12 @@ async function draw() {
   const slices = populationPie(dataset);
 
   const arc = d3.arc().outerRadius(radius).innerRadius(0);
+
+  const colors = d3.quantize(d3.interpolateSpectral, dataset.length);
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain(dataset.map((element) => element.name))
+    .range(["#ef66ef", "#00766d", "#909090", "#00ffd2", "#fff"]);
   //Draw Shape
   const arcGroup = ctr
     .append("g")
@@ -39,7 +54,12 @@ async function draw() {
       "transform",
       `translate(${dimensions.ctrHeight / 2}, ${dimensions.ctrWidth / 2})`
     );
-  arcGroup.selectAll("path").data(slices).join("path").attr("d", arc);
+  arcGroup
+    .selectAll("path")
+    .data(slices)
+    .join("path")
+    .attr("d", arc)
+    .attr("fill", (d) => colorScale(d.data.name));
 }
 
 draw();
